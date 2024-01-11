ae60622a76345531fcef1ed519f93e1b534cbed7
Your Name <you@example.com>
2024-01-11 04:05:57 -0800

    initial route framework

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/mean', function(req, res) {
+
+});
+
+app.get('/median', function(re, res) {
+
+});
+
+app.get('/mode', function(re, res) {
+
+});
