90d3639762f8fea0ff216244adab57e6217678b4
Your Name <you@example.com>
2024-01-13 10:41:34 -0800

    add app.listen

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/mean', function(req, res) {
+app.get('/mean?:nums', function(req, res) {
+    return res.json(
+        { nums: req.params.nums,
+          mean:  
+     });
 
 });
 
-app.get('/median', function(re, res) {
+app.get('/median?:nums', function(re, res) {
 
 });
 
-app.get('/mode', function(re, res) {
+app.get('/mode?;nums', function(re, res) {
 
+});
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000.');
 });
