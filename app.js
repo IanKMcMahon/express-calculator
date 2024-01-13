7bf38a87ad13a35a05cb1623de9689b6c41f8d05
Your Name <you@example.com>
2024-01-13 11:39:41 -0800

    amend /mean route

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/mean?:nums', function(req, res) {
-let mean = Math().mean([nums])
-console.log(mean)
-return mean
-     });
+app.get('/mean', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
 
+
+  let result = {
+    operation: "mean",
+    result: findMean(nums)
+  }
+
+  return res.send(result);
+});
 
 
 app.get('/median?:nums', function(req, res) {
