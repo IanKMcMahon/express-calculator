ee4867d695103ae5a17a1d9bb20fe9df921d01b2
Your Name <you@example.com>
2024-01-13 12:05:13 -0800

    median function

--- app.js
+++ app.js
@@ -28,9 +28,24 @@ app.get('/mean', function(req, res, next) {
 });
 
 
-app.get('/median?:nums', function(req, res) {
-  let median = Math().median([nums])
-  return mean
+app.get('/median', function(req, res,next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums)
+  }
+
+  return res.send(result);
 });
 
 app.get('/mode?;nums', function(req, res) {
