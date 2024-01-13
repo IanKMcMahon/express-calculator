3917e4475a64bb7a926d58901fddda98c460bb57
Your Name <you@example.com>
2024-01-13 12:06:35 -0800

    mode route

--- app.js
+++ app.js
@@ -48,9 +48,24 @@ app.get('/median', function(req, res,next) {
   return res.send(result);
 });
 
-app.get('/mode?;nums', function(req, res) {
-  let mode = Math().mode([nums])
-  return mean
+app.get('/mode?;nums', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums)
+  }
+
+  return res.send(result);
 });
 
 app.listen(3000, function() {
